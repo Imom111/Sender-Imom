d7b7fa88f3737764b34a641d62dd947a879682ff
Iván Ornelas <imom11ivan@gmaiil.com>
2022-10-11 23:25:04 -0500

    Array example update

--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,34 @@
+let arrExample = [
+  {
+    "id" : 1,
+    "email" : "ivan@gmail.com",
+    "name" : "Ivan",
+    "enterprise" : "IMOM-JS",
+    "address" : "León Guanajuato"
+  },
+  {
+    "id" : 2,
+    "email" : "moi@gmail.com",
+    "name" : "Moi",
+    "enterprise" : "MOI-JS",
+    "address" : "Silao Guanajuato"
+  },
+  {
+    "id" : 3,
+    "email" : "ornelas@gmail.com",
+    "name" : "Ornelas",
+    "enterprise" : "Orn-JS",
+    "address" : "Guanajuato Guanajuato"
+  },
+  {
+    "id" : 4,
+    "email" : "meza@gmail.com",
+    "name" : "Meza",
+    "enterprise" : "Mez-JS",
+    "address" : "DF"
+  }
+];
+
 window.addEventListener('DOMContentLoaded', (event) => {
   init();
 });
@@ -15,4 +46,17 @@ function init(){
   default_message += 'Sincerely Iván Ornelas.';
   
   $("#message_body").val(default_message);
+}
+
+function load_receivers(){
+  let content = "";
+  for (let i = 0; i < arrExample.length; i++) {
+    content += '<tr>';
+    content += '<td>' + arrExample[i].email + '</td>';
+    content += '<td>' + arrExample[i].name + '</td>';
+    content += '<td><a type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-dismiss="modal" data-bs-target="#edit_data_receiver_modal">Edit</a></td>';
+    content += '<td><a type="button" class="btn btn-danger text-white">Delete</a></td>';
+    content += '</tr>';
+  }
+  $("#tbody_receiver").html();
 }
